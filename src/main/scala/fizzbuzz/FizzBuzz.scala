c94f968c062badf4a826f6d6c7470dd380817507
Brian Balser <brian.balser@gmail.com>
2014-10-07 14:39:52 -0400

    Refactored to use Object unapply methods in pattern match

--- src/main/scala/fizzbuzz/FizzBuzz.scala
+++ src/main/scala/fizzbuzz/FizzBuzz.scala
@@ -7,9 +7,17 @@ object FizzBuzz {
   }
 
   private def convertNumber(num: Int) : String  = num match {
-    case x if (x % 3 == 0) => "fizz"
-    case x if (x % 5 == 0) => "buzz"
+    case IsFizz() => "fizz"
+    case IsBuzz() => "buzz"
     case x => x.toString
+  }
+
+  object IsFizz {
+    def unapply(num: Int): Boolean = num % 3 == 0
+  }
+
+  object IsBuzz {
+    def unapply(num: Int): Boolean = num % 5 == 0
   }
 
 }
