a20c3ed2e5027e2bd523b6d562474d5bcc70c687
Brian Balser <brian.balser@gmail.com>
2014-10-07 12:54:04 -0400

    Passed test by added else if clause

--- src/main/scala/fizzbuzz/FizzBuzz.scala
+++ src/main/scala/fizzbuzz/FizzBuzz.scala
@@ -3,7 +3,11 @@ package fizzbuzz
 object FizzBuzz {
 
   def fizzbuzz(numbers: List[Int]): List[String] = {
-      numbers.map { x => if (x == 3)  "fizz" else x.toString }
+      numbers.map { x =>
+        if (x == 3) "fizz"
+        else if (x == 5) "buzz"
+        else x.toString
+      }
   }
 
 }
