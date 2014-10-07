65123defad93ca53efdfe7048b5e1ee9823389bd
Brian Balser <brian.balser@gmail.com>
2014-10-07 10:53:31 -0400

    Passed failing test by changing default case to convert entire list to strings

--- src/main/scala/fizzbuzz/FizzBuzz.scala
+++ src/main/scala/fizzbuzz/FizzBuzz.scala
@@ -5,8 +5,7 @@ object FizzBuzz {
   def fizzbuzz(numbers: List[Int]): List[String] = {
     numbers match {
       case 3::_ => List("fizz")
-      case 2::_ => List("2")
-      case _ => List("1")
+      case _ => numbers.map { x => x.toString }
     }
   }
 
