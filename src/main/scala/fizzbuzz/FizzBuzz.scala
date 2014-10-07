5e1fc4d3bc3c6e547027aa1495fe5ffe1d08beee
Brian Balser <brian.balser@gmail.com>
2014-10-07 12:56:12 -0400

    Refactor to match pattern and moved to private method

--- src/main/scala/fizzbuzz/FizzBuzz.scala
+++ src/main/scala/fizzbuzz/FizzBuzz.scala
@@ -3,11 +3,13 @@ package fizzbuzz
 object FizzBuzz {
 
   def fizzbuzz(numbers: List[Int]): List[String] = {
-      numbers.map { x =>
-        if (x == 3) "fizz"
-        else if (x == 5) "buzz"
-        else x.toString
-      }
+      numbers.map(convertNumber)
+  }
+
+  private def convertNumber(num: Int) : String = num match {
+    case 3 => "fizz"
+    case 5 => "buzz"
+    case _ => num.toString
   }
 
 }
