35662306151e60ff83b2afccc2f9727afcd44532
Brian Balser <brian.balser@gmail.com>
2014-10-07 09:52:30 -0400

    Added failing test for number 2

--- src/test/scala/fizzbuzz/FizzBuzzSpec.scala
+++ src/test/scala/fizzbuzz/FizzBuzzSpec.scala
@@ -5,8 +5,12 @@ import fizzbuzz.FizzBuzz.fizzbuzz
 
 class FizzBuzzSpec extends FlatSpec with ShouldMatchers {
 
-  "FizzBuzz" should "return List of 1 when give List of 1" in {
+  "FizzBuzz" should "return List of 1 when given List of 1" in {
     fizzbuzz(List(1)) should be (List("1"))
+  }
+
+  it should "return List of 2 when given List of 2" in {
+    fizzbuzz(List(2)) should be (List("2"))
   }
 
   it should "return List of fizz when given List of number divisible by 3" in {
