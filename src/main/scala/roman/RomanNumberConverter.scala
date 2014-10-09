af7c755bb9fc3cc727d9e1a5527d9862ff9d66ea
Brian Balser <brian.balser@gmail.com>
2014-10-08 23:11:28 -0400

    Passed test and removed unnecessary code

--- src/main/scala/roman/RomanNumberConverter.scala
+++ src/main/scala/roman/RomanNumberConverter.scala
@@ -7,10 +7,11 @@ object RomanNumberConverter {
   def toRoman(arabic: Int): String = {
 
     @tailrec def convertToRoman(remainder: Int, roman: String) : String = {
-      if (remainder == 10){
-        convertToRoman(remainder-10,roman + "X")
-      }
-      else if (remainder >= 5) {
+      if (remainder == 50) {
+        "L"
+      } else if (remainder == 10) {
+        "X"
+      } else if (remainder >= 5) {
         convertToRoman(remainder-5,roman + "V")
       } else {
         roman + ("I" * remainder)
