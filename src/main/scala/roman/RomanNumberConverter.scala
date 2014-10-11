44156701bf46f03162db5607981f46d1445b686f
bbalser <brian.balser@gmail.com>
2014-10-10 20:45:48 -0400

    Passed test for 9

--- src/main/scala/roman/RomanNumberConverter.scala
+++ src/main/scala/roman/RomanNumberConverter.scala
@@ -3,10 +3,9 @@ package roman
 import scala.annotation.tailrec
 
 object RomanNumberConverter {
-  val data = List( (100,"C"), (50, "L"), (10, "X"), (5, "V"),(4, "IV"), (1, "I"))
+  val data = List((100, "C"), (50, "L"), (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I"))
 
   def toRoman(arabic: Int): String = {
-
 
     data.foldLeft((arabic, "")) { case ((number, buffer), (arabicValue, romanCharacter)) =>
       (number % arabicValue, buffer + (romanCharacter * (number / arabicValue)))
@@ -14,8 +13,8 @@ object RomanNumberConverter {
 
   }
 
-  def testFoldLeft(in: Int): Int ={
-    val list = List(1,2,3,4,5,6)
+  def testFoldLeft(in: Int): Int = {
+    val list = List(1, 2, 3, 4, 5, 6)
     list.foldLeft(in)((thisIsInOrTheResultOfThePrevIteration, thisIsAnElementFromTheList) => thisIsInOrTheResultOfThePrevIteration + thisIsAnElementFromTheList)
   }
 
--- src/test/scala/roman/RomanNumberConverterSpec.scala
+++ src/test/scala/roman/RomanNumberConverterSpec.scala
@@ -7,50 +7,53 @@ import roman.RomanNumberConverter.testFoldLeft
 class RomanNumberConverterSpec extends FlatSpec with ShouldMatchers {
 
   "When converting arabic to roman numerals, it" should "return I for 1" in {
-    toRoman(1) should be ("I")
+    toRoman(1) should be("I")
   }
 
   it should "return II for 2" in {
-    toRoman(2) should be ("II")
+    toRoman(2) should be("II")
   }
 
   it should "return IV for 4" in {
-    toRoman(4) should be ("IV")
+    toRoman(4) should be("IV")
   }
 
   it should "return V for 5" in {
-    toRoman(5) should be ("V")
+    toRoman(5) should be("V")
   }
+
   it should "return VI for 6" in {
-    toRoman(6) should be ("VI")
+    toRoman(6) should be("VI")
   }
+
   it should "return IX for 9" in {
-    toRoman(9) should be ("IX")
+    toRoman(9) should be("IX")
   }
+
   it should "return X for 10" in {
-    toRoman(10) should be ("X")
+    toRoman(10) should be("X")
   }
 
   it should "return XI for 11" in {
-    toRoman(11) should be ("XI")
+    toRoman(11) should be("XI")
   }
 
   it should "return L for 50" in {
-    toRoman(50) should be ("L")
+    toRoman(50) should be("L")
   }
 
   it should "return LI for 51" in {
-    toRoman(51) should be ("LI")
+    toRoman(51) should be("LI")
   }
 
   it should "return C for 100" in {
-    toRoman(100) should be ("C")
+    toRoman(100) should be("C")
   }
 
 
 
   it should "return 21 for list(1,2,3,4,5,6)" in {
-    testFoldLeft(0) should be (21)
+    testFoldLeft(0) should be(21)
   }
 
 }
