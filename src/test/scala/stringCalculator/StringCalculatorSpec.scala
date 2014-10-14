f7678da48cf1e1416fb1d7cf9a5e44d979ab967b
bbalser <brian.balser@gmail.com>
2014-10-14 13:02:10 -0400

    Added failing test for "1"

--- src/test/scala/stringCalculator/StringCalculatorSpec.scala
+++ src/test/scala/stringCalculator/StringCalculatorSpec.scala
@@ -6,8 +6,12 @@ import stringCalculator.StringCalculator.add
 
 class StringCalculatorSpec extends FlatSpec with ShouldMatchers {
 
-  "When adding strings, it" should "return 0 for empty string" in{
+  "When adding strings, it" should "return 0 for empty string" in {
     add("") should be (0)
+  }
+
+  it should "return 1 when given String 1" in {
+    add("1") should be (1)
   }
 
 
