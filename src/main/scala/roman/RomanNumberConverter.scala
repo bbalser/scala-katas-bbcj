0fd9370c05530e5af8097147b23955c75c7f5da6
bbalser <brian.balser@gmail.com>
2014-10-13 21:25:46 -0400

    Passed test for 4, had to refactor to recursive solution just to get the pass

--- src/main/scala/roman/RomanNumberConverter.scala
+++ src/main/scala/roman/RomanNumberConverter.scala
@@ -1,5 +1,7 @@
 package roman
 
+import scala.annotation.tailrec
+
 object RomanNumberConverter {
   val data = List((100, "C"), (50, "L"), (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I"))
 
@@ -12,11 +14,16 @@ object RomanNumberConverter {
   }
 
   def toArabic(roman: String): Int = {
-    var arabicResult = 0
 
-    for (char <- roman ; pair <- data if pair._2 == char.toString)  arabicResult += pair._1
+    @tailrec def innerConvert(romanString: String, arabicValue: Int): Int = romanString match {
+      case "" => arabicValue
+      case x => {
+        val tuple = data.find { case (arabicValue, romanCharacter) => romanString.startsWith(romanCharacter)}.get
+        innerConvert(romanString.drop(tuple._2.size), arabicValue + tuple._1)
+      }
+    }
 
-    arabicResult
+    innerConvert(roman, 0)
   }
 
 
