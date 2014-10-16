969c6c20ef4504878f9411cbca5a476c5f653e0d
bbalser <brian.balser@gmail.com>
2014-10-16 09:24:06 -0400

    Passed test with custom delimiter

--- src/main/scala/stringCalculator/StringCalculator.scala
+++ src/main/scala/stringCalculator/StringCalculator.scala
@@ -4,7 +4,8 @@ package stringCalculator
 object StringCalculator {
 
   def add(numbers: String): Int = {
-    numbers.replace("\n", ",").split(",").collect { case x if !x.isEmpty => x.toInt }.sum
+    val cleanedNumbers = ";|/".r.replaceAllIn(numbers, "")
+    cleanedNumbers.replace("\n", ",").split(",").collect { case x if !x.isEmpty => x.toInt }.sum
   }
 
 }
