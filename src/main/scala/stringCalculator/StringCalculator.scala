3e75a87a5d4fd378ca04e578fa8efea2bfe9e46d
bbalser <brian.balser@gmail.com>
2014-10-21 22:05:24 -0400

    Passed test for filter for !- 1001

--- src/main/scala/stringCalculator/StringCalculator.scala
+++ src/main/scala/stringCalculator/StringCalculator.scala
@@ -11,9 +11,8 @@ object StringCalculator {
     val cleanedNumbers = "/".r.replaceAllIn(numbers, "")
     val numberList = cleanedNumbers.replace("\n", delimiter).split(delimiter).toList.collect { case x if !x.isEmpty => x.toInt}
 
-
     numberList.filter(_ < 0) match {
-      case Nil => numberList.sum
+      case Nil => numberList.filter(_ != 1001).sum
       case x => throw new IllegalArgumentException(s"negatives not allowed : ${x.mkString(", ")}")
     }
   }
