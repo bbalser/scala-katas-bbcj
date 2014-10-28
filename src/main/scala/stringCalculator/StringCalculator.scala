156f67664d60bbfb468eb206d9e3a2ed643d452e
Chad James <ChadJames@columbus.rr.com>
2014-10-27 20:24:17 -0400

    Passed test for multiple newlines.

--- src/main/scala/stringCalculator/StringCalculator.scala
+++ src/main/scala/stringCalculator/StringCalculator.scala
@@ -2,11 +2,11 @@ package stringCalculator
 
 object StringCalculator {
 
-  val CustomDelimiterRegex = "^//(.+)\n.\n*.*$".r
+  val CustomDelimiterRegex = "^//(.+)$".r
 
   def add(numbers: String): Int = {
 
-    val delimiter = determineDelimiter(numbers)
+    val delimiter = if (numbers.startsWith("//")) determineDelimiter((numbers.split("\n")(0))) else ","
 
     val cleanedNumbers = "/".r.replaceAllIn(numbers, "")
     val numberList = cleanedNumbers.replace("\n", delimiter).split(delimiter).toList.collect { case x if !x.isEmpty => x.toInt}
@@ -19,8 +19,9 @@ object StringCalculator {
 
   def determineDelimiter(numbers: String): String = numbers match {
     case CustomDelimiterRegex(x) => x
-    case _ => ","
+
   }
+
 
 }
 
