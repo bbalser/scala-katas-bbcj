4cc1704fec2e088ce857d5a63fdfda4ab373ee55
chadjames <chadjames@columbus.rr.com>
2014-10-29 15:45:48 -0400

    Passed test of NICKLE.

--- src/main/scala/vendingMachine/VendingMachine.scala
+++ src/main/scala/vendingMachine/VendingMachine.scala
@@ -2,12 +2,16 @@ package vendingMachine
 
 
 class VendingMachine {
+  var credit: String = ""
 
-  def display: String = {
-    "INSERT COIN"
+  def display: String =  credit match{
+    case "" => "INSERT COIN"
+    case _ =>  "0.05"
   }
 
-  def insert(coin: String) = ???
+  def insert(coin: String) = {
+    credit = coin
+  }
 
 
 
