5aff748427a98ddc01205da606e539a685151073
bbalser <brian.balser@gmail.com>
2014-10-30 08:41:36 -0400

    Passed 2 quarter test by adding more conditional checks on insert of coin

--- src/main/scala/vendingMachine/VendingMachine.scala
+++ src/main/scala/vendingMachine/VendingMachine.scala
@@ -10,12 +10,14 @@ class VendingMachine {
     case "QUARTER" => "0.25"
     case "2 NICKELS" => "0.10"
     case "2 DIMES" => "0.20"
+    case "2 QUARTERS" => "0.50"
     case _ =>  "0.05"
   }
 
   def insert(coin: String) = coin match{
     case "NICKEL" => credit = if (credit == "NICKEL") "2 NICKELS" else coin
     case "DIME" => credit = if (credit == "DIME") "2 DIMES" else coin
+    case "QUARTER" => credit = if (credit == "QUARTER") "2 QUARTERS" else coin
     case _ => credit = coin
 
 
