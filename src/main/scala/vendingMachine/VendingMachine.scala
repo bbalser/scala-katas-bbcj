c39f44a35ed06f9ef3cdabe3f559337d89fd6d66
bbalser <brian.balser@gmail.com>
2014-10-30 11:48:01 -0400

    Passed test by refactoring logic to handle unknown coins on insert

--- src/main/scala/vendingMachine/VendingMachine.scala
+++ src/main/scala/vendingMachine/VendingMachine.scala
@@ -6,19 +6,24 @@ import java.text.DecimalFormat
 class VendingMachine {
   var credit: Option[Double] = None
 
-
   def display: String =  credit.map( new DecimalFormat("0.00").format(_)).getOrElse("INSERT COIN")
 
-  def insert(coin: String) = credit = Some(credit.getOrElse(0.0) + (coin match {
-    case "NICKEL" => 0.05
-    case "DIME" => 0.1
-    case "QUARTER" => 0.25
-    case _ => 0.0
-  }))
+  def insert(coin: String) = credit = coin match {
+    case "NICKEL" => addToCredit(0.05)
+    case "DIME" => addToCredit(0.1)
+    case "QUARTER" => addToCredit(0.25)
+    case _ => credit
+  }
 
   def coinReturn: List[String] = {
-    credit = None
     List("PENNY")
+  }
+
+  def addToCredit(value: Double): Option[Double] = {
+    credit match {
+      case None => Some(value)
+      case Some(x) => Some(x + value)
+    }
   }
 
 }
