16bc568a1f7128d8d3666a12e2a736c15e9cf64f
bbalser <brian.balser@gmail.com>
2014-10-30 09:01:05 -0400

    Refactored to get rid of conditionals and incorporate an Option

--- src/main/scala/vendingMachine/VendingMachine.scala
+++ src/main/scala/vendingMachine/VendingMachine.scala
@@ -1,26 +1,17 @@
 package vendingMachine
 
+import java.text.DecimalFormat
+
 
 class VendingMachine {
-  var credit: String = ""
+  var credit: Option[Double] = None
 
-  def display: String =  credit match{
-    case "" => "INSERT COIN"
-    case "DIME" => "0.10"
-    case "QUARTER" => "0.25"
-    case "2 NICKELS" => "0.10"
-    case "2 DIMES" => "0.20"
-    case "2 QUARTERS" => "0.50"
-    case _ =>  "0.05"
-  }
+  def display: String =  credit.map( new DecimalFormat("0.00").format(_)).getOrElse("INSERT COIN")
 
-  def insert(coin: String) = coin match{
-    case "NICKEL" => credit = if (credit == "NICKEL") "2 NICKELS" else coin
-    case "DIME" => credit = if (credit == "DIME") "2 DIMES" else coin
-    case "QUARTER" => credit = if (credit == "QUARTER") "2 QUARTERS" else coin
-    case _ => credit = coin
-
-
-  }
+  def insert(coin: String) = credit = Some(credit.getOrElse(0.0) + (coin match {
+    case "NICKEL" => 0.05
+    case "DIME" => 0.1
+    case "QUARTER" => 0.25
+  }))
 
 }
