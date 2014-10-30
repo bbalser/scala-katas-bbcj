dd38fbe56502df8f7ca7b2a8eaa5c38b220953a4
chadjames <chadjames@columbus.rr.com>
2014-10-30 15:56:26 -0400

    Passed test for half dollar.

--- src/main/scala/vendingMachine/VendingMachine.scala
+++ src/main/scala/vendingMachine/VendingMachine.scala
@@ -5,6 +5,7 @@ import java.text.DecimalFormat
 
 class VendingMachine {
   var credit: Option[Double] = None
+  var coinReturn: List[String] = Nil
 
   def display: String =  credit.map( new DecimalFormat("0.00").format(_)).getOrElse("INSERT COIN")
 
@@ -12,11 +13,7 @@ class VendingMachine {
     case "NICKEL" => addToCredit(0.05)
     case "DIME" => addToCredit(0.1)
     case "QUARTER" => addToCredit(0.25)
-    case _ => credit
-  }
-
-  def coinReturn: List[String] = {
-    List("PENNY")
+    case _ => reject(coin)
   }
 
   def addToCredit(value: Double): Option[Double] = {
@@ -24,6 +21,10 @@ class VendingMachine {
       case None => Some(value)
       case Some(x) => Some(x + value)
     }
+  }
+  def reject(coin: String) : Option[Double] = {
+    coinReturn = coin :: coinReturn
+    credit
   }
 
 }
