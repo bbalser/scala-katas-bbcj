bf2cdfec8f09e8f61e7d88914784c3725f95af4a
bbalser <brian.balser@gmail.com>
2014-11-03 20:43:27 -0500

    Small refactor to turn productSelected into an Option

--- src/main/scala/vendingMachine/VendingMachine.scala
+++ src/main/scala/vendingMachine/VendingMachine.scala
@@ -6,10 +6,13 @@ import java.text.DecimalFormat
 class VendingMachine {
   var credit: Option[Double] = None
   var coinReturn: List[String] = Nil
-  var productSelected = ""
+  var productSelected: Option[String] = None
   var priceList = Map("COLA" -> 1.00, "CHIPS" -> 0.50, "CANDY" -> 0.65)
 
-  def display: String =  if (productSelected != "") "PRICE: " + formatDecimal(Option(priceList(productSelected))).get else formatDecimal(credit).getOrElse("INSERT COIN")
+  def display: String = productSelected match {
+    case Some(x) => "PRICE: " + formatDecimal(priceList(x))
+    case None => formatDecimal(credit).getOrElse("INSERT COIN")
+  }
 
   def insert(coin: String): Unit = Coin.decode(coin) match {
     case Some(x) => credit = addToCredit(x.value)
@@ -20,8 +23,10 @@ class VendingMachine {
     credit.map(_ + value) orElse Some(value)
   }
 
-  private def formatDecimal(decimal: Option[Double]) = decimal.map( new DecimalFormat("0.00").format )
+  private def formatDecimal(decimal: Option[Double]): Option[String] = decimal.map( formatDecimal )
 
-  def selectProduct(value: String) = productSelected = value
+  private def formatDecimal(decimal: Double): String = new DecimalFormat("0.00").format(decimal)
+
+  def selectProduct(value: String) = productSelected = Some(value)
 
 }
