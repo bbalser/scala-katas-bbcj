b768a70bbd98801b3b8fff552eb76e2eb946fd66
bbalser <brian.balser@gmail.com>
2014-11-17 21:27:01 -0500

    Passed test by recursive algorithm to determine next coin. Also change tracking of inserting money to int representing cents to avoid double precision issuesy

--- src/main/scala/vendingMachine/Coin.scala
+++ src/main/scala/vendingMachine/Coin.scala
@@ -1,16 +1,16 @@
 package vendingMachine
 
-sealed class Coin(val name: String, val value: Double)
+sealed class Coin(val name: String, val value: Int)
 
 object Coin {
 
   private val coins: List[Coin] = List(NICKEL, DIME, QUARTER)
 
   def decodeName(name: String): Option[Coin] = coins.find { coin => coin.name == name }
-  def decodeValue(value: String): Option[Coin] = coins.find { coin => VendingMachine.formatDecimal(coin.value) == value }
+  def decodeValue(value: String): Option[Coin] = coins.find { coin => VendingMachine.formatCents(coin.value) == value }
 
-  case object NICKEL extends Coin("NICKEL", 0.05)
-  case object DIME extends Coin("DIME", 0.1)
-  case object QUARTER extends Coin("QUARTER", 0.25)
+  case object NICKEL extends Coin("NICKEL", 5)
+  case object DIME extends Coin("DIME", 10)
+  case object QUARTER extends Coin("QUARTER", 25)
 
 }
--- src/main/scala/vendingMachine/VendingMachine.scala
+++ src/main/scala/vendingMachine/VendingMachine.scala
@@ -2,49 +2,61 @@ package vendingMachine
 
 import java.text.DecimalFormat
 
-class VendingMachine()(implicit screen: DisplayScreen = new DisplayScreen(),
-                                coinReturn: CoinReturn = new CoinReturn(),
-                                hopper: Hopper = new Hopper()) {
+import vendingMachine.Coin.{QUARTER, DIME, NICKEL}
 
-  var credit: Double = 0
+import scala.annotation.tailrec
+
+class VendingMachine()(implicit screen: DisplayScreen = new DisplayScreen(),
+                       coinReturn: CoinReturn = new CoinReturn(),
+                       hopper: Hopper = new Hopper()) {
+
+  var cents: Int = 0
 
   screen.display("INSERT COIN")
 
   def insert(coin: String): Unit = Coin.decodeName(coin) match {
     case Some(x) => {
-      credit += x.value
-      screen.display(VendingMachine.formatDecimal(credit))
+      cents += x.value
+      screen.display(VendingMachine.formatCents(cents))
     }
     case None => coinReturn.returnCoins(coin)
   }
-
 
 
   def selectProduct(value: String) = {
     var output = ""
     val price = VendingMachine.priceList(value)
 
-    if (credit >= price){
+    if (cents >= price) {
       hopper.add(value)
-      output =  "THANK YOU"
-    }else{
-      output = s"PRICE: ${VendingMachine.formatDecimal(price)}"
+      output = "THANK YOU"
+    } else {
+      output = s"PRICE: ${VendingMachine.formatCents(price)}"
     }
     makeChange(price)
     screen.display(output)
   }
 
   def makeChange(price: Double) = {
-      Coin.decodeValue(VendingMachine.formatDecimal(credit - price)) match {
-        case Some(x) =>  coinReturn.returnCoins(x.name)
-        case None => ""
+
+    @tailrec def determineNextCoin(remaining: Double, change: List[Coin]): List[Coin] = {
+      List(QUARTER, DIME, NICKEL).find { x => remaining >= x.value } match {
+        case Some(nextCoin) => determineNextCoin(remaining - nextCoin.value, change :+ nextCoin)
+        case None => change
+      }
+    }
+
+    val change = determineNextCoin(cents - price, Nil)
+    if (!change.isEmpty) {
+      coinReturn.returnCoins(change.map(_.name).mkString(","))
     }
   }
 
 }
 
 object VendingMachine {
-  val priceList = Map("COLA" -> 1.00, "CHIPS" -> 0.50, "CANDY" -> 0.65)
-  def formatDecimal(decimal: Double): String = new DecimalFormat("0.00").format(decimal)
+  val priceList = Map("COLA" -> 100, "CHIPS" -> 50, "CANDY" -> 65)
+
+  def formatCents(cents: Int): String = new DecimalFormat("0.00").format(cents / 100.0)
 
 }
